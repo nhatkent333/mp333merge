5ceb51c7c18237a1bfbc8aeede9d6c2e9819681e
nhatkent333 <89648281+nhatkent333@users.noreply.github.com>
2024-01-28 16:50:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
-import io
-import pathlib
-
-import streamlit as st
-from pydub import AudioSegment
-
-st.title('MP3 Merger')
-
-st.markdown("""This is a quick example app for merging two MP3 files using the **pydub** audio library on Streamlit Cloud.
+st.markdown("""This is a quick example app for merging multiple MP3 files using the **pydub** audio library on Streamlit Cloud.
 There are some issues with `ffmpeg` on Streamlit Cloud regarding temporary files and file permissions.
 The quick fix is to use `libav` instead of `ffmpeg` in `packages.txt` file, because pydub prefers `libav` over `ffmpeg` if it is installed.
 Therefore this example app uses `libav`.""")
 
-uploaded_mp3_file1 = st.file_uploader('Upload Your First MP3 File', type=['mp3'])
-uploaded_mp3_file2 = st.file_uploader('Upload Your Second MP3 File', type=['mp3'])
+uploaded_files = st.file_uploader('Upload up to 5 MP3 Files', type=['mp3'], accept_multiple_files=True)
 
-if uploaded_mp3_file1 and uploaded_mp3_file2:
-    audio1 = AudioSegment.from_file(uploaded_mp3_file1)
-    audio2 = AudioSegment.from_file(uploaded_mp3_file2)
-    merged_audio = audio1 + audio2
+if uploaded_files:
+    merged_audio = AudioSegment.empty()
+    for uploaded_file in uploaded_files:
+        audio = AudioSegment.from_file(uploaded_file)
+        merged_audio += audio
     merged_audio.export("merged.mp3", format='mp3')
     st.success('Files merged successfully!')
 
